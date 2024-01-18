a1b34889d398f19ac714e9d564403307f6e4af7d
brofrain <knwelc@gmail.com>
2024-01-18 20:55:17 +0100

    feat: update examples

--- examples/ssr/src/lib.rs
+++ examples/ssr/src/lib.rs
@@ -1,22 +1,17 @@
-use cfg_if::cfg_if;
-
 pub mod app;
 mod button;
+
+#[cfg(feature = "ssr")]
 pub mod fallback;
 
-cfg_if! { if #[cfg(feature = "hydrate")] {
-    use leptos::{view, wasm_bindgen};
-    use wasm_bindgen::prelude::wasm_bindgen;
+#[cfg(feature = "hydrate")]
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn hydrate() {
     use crate::app::App;
 
-    #[wasm_bindgen]
-    pub fn hydrate() {
-        // initializes logging using the `log` crate
-        _ = console_log::init_with_level(log::Level::Debug);
-        console_error_panic_hook::set_once();
+    // initializes logging using the `log` crate
+    _ = console_log::init_with_level(log::Level::Debug);
+    console_error_panic_hook::set_once();
 
-        leptos::mount_to_body(move || {
-            view! { <App/> }
-        });
-    }
-}}
+    leptos::mount_to_body(App);
+}
